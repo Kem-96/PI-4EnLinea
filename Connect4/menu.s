f623604f1819bea1abce3aaf75bf7929038db49c
Porgois <isaacvargasjimenez20@ucr.ac.cr>
2022-10-18 20:25:04 -0600

    agregadas reglas al menu.

--- Connect4/menu.s
+++ Connect4/menu.s
@@ -14,6 +14,9 @@ len: equ $-msg
 msg1: db "Digite una opcion: "
 len1: equ $-msg1
 
+reglas: db "Las reglas del juego consisten en que cada jugador coloca una ficha en la columna que desee hasta que 1 de 2 opciones se cumplan: ", 0xa, "[1]Cualquiera de los jugadores gane.", 0xa, "[2]El tablero se llene.",0xa, "La ficha se va a colocar en la primera posicion sin ocupar, de abajo hacia arriba.", 0xa
+reglas_len: equ $-reglas
+
 integrantes: db "---Integrantes---", 0xa, "Nathan Murillo (B95506)", 0xa, "Rodrigo Mendoza (C04813)", 0xa, "Kembly Paniagua (B95822)", 0xa,  "Isaac Vargas (C08195)", 0xa
 lenInte: equ $-integrantes
 
@@ -47,6 +50,9 @@ compares:
 
   cmp al, 2
   je companeros ;salto a integrantes, opcion 2
+
+  cmp al, 3
+  je imprimir_reglas ;salto a reglas, opcion 3
   
   cmp al, 4 ;salto a exit, opcion 4
   je salir
@@ -60,6 +66,18 @@ companeros:
   print integrantes, lenInte
   print volver, lenVolver
   
+  input opcion, 2
+  printNl
+  ind r8, opcion
+  
+  cmp al, 5
+  je menu
+  ret
+
+imprimir_reglas: 
+  print reglas, reglas_len
+  print volver, lenVolver
+
   input opcion, 2
   printNl
   ind r8, opcion
