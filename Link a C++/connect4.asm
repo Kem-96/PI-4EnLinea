eef086cd62ce23b4c18b12720aa63793ae3634f8
WarMa <kali_1103@hotmail.es>
2022-11-09 11:22:06 -0600

    Corrección de algunos errores de truncated location.
    Aun conserva errores pero en menor cantidad.

--- Link a C++/connect4.asm
+++ Link a C++/connect4.asm
@@ -5,48 +5,87 @@ global inicio
 
 section .data
 	extern tablero
+	extern columna
+	extern jugador
+	extern indice
+	extern casillasLlenas
 	
 section .bss
 	match: resb 2
     valor: resb 2
     tempPosix: resb 2
     TheChoosenJuan: resb 2
-	extern columna
-	extern jugador
-	extern indice
-	extern casillasLlenas
+	swap: resb 1
 	
 section .txt
+	extern imp
+	extern imp1
+	extern ExitProcess
 
 inicio:
 
+	call imp
+	mov r8b, [jugador]
+	add r8b, 48
+	mov [jugador], r8b
 turn:
-  cmp [jugador], byte 1
+  mov r8b, [jugador]
+  cmp r8b, 49
   je two
-  cmp [jugador], byte 2
-  je one
   
+  ;cl contiene el parametro jugador...  
+one:
+  mov [jugador], byte 49
+  jmp continue ;jmp continue
+two:
+  mov [jugador], byte 50
+
 continue:
+  mov r8b, [jugador]
+ 
+;xor r11, r11
+;xor rax,rax
+;lea r11, [tablero]
+;mov al, [columna]
+;mov rax, 76
+;mov [r11+rax], byte 49
+;cmp [r11+rax], byte 48
+;add al, 1
+;cmp byte [columna],  byte 12
+;je algo
+;jmp fin
+;algo:
+;call imp
+
+;ret
   ;tomar input de la columna
   
   ;Corrige la posicion del indice, ya que "0 0 0" ;hay espacios en las filas,asi que si se inserta en la columna 3, el valor correcto seria el 6.
+  xor r9, r9
+  xor rax, rax
   mov r9b, [columna]	
   mov al, 2
   mul r9
-  mov r9b, al
-  mov r8b, [jugador]	
+  push rax
+  ;-mov r9b, al
+  ;mov r8b, [jugador]	
   
 ;Multiplica el indice por 14 (que inicia de 5 a 0) y suma la columna ingresada para determinar la posición del tableto, ej, indice 5, columna 0 daria la posición (14*5)+0 = 70, que es la esquina inferior izquierda...
 for:
+  xor rax, rax
+  xor r10, r10
   mov al, [indice]		
   mov r10b, 14
   mul r10b
   mov r10b, al
-  add r10b, r9b
-  mov al, [tablero+r10]
-  cmp al, "0"
+  pop rax
+  add r10b, al
+  mov al, r10b
+  lea r11, [tablero]
+  ;add r11, r10
+  ;jmp set------------------------- de acapabajo hay un offset invalido...
+  cmp [r11+rax], byte 48
   je set
-  call aux 
   
 aux:
   mov al, [indice]
@@ -58,14 +97,25 @@ aux:
   jl columnaLlena
   
 set:
-  mov [tablero+r10], r8b
+  lea r11, [tablero]
+  mov [r11+rax], r8b
+  ;mov [tablero+r10], r8b----------------------------------------
+  call imp
   mov [tempPosix], r10
   call resetIndice
+  call imp
   ;call impresion 			Actualizar tablero...
   mov al, [casillasLlenas]
   inc al
   mov [casillasLlenas], al
+  ;problema en comprobarGanador....
   call comprobarGanador
+  
+columnaLlena:
+  call resetIndice
+  ;print msgLleno, lenLleno
+  call impresion
+  jmp continue
   
 continueSet:
   call resetMatch
@@ -82,31 +132,44 @@ resetMatch:
   ret
   
 comprobarGanador:
-  mov rax, [tempPosix]
+  xor rax, rax
+  mov al, [tempPosix]
+  
+  
   
   izquierdaAsc:
   mov r8, izquierdaAsc
   mov r9, izquierdaDesc
   sub rax, 15
-  mov r11, [tablero+rax]
-  mov [valor], r11b
+  
+  ;mov r11, [tablero+rax]------------------
+  lea r11, [tablero]
+  push rcx
+  mov rcx, [r11+rax]
+  mov rax, rcx
+  pop rcx
+  mov [valor], byte al
   cmp byte [valor], 10
   je auxJump
   dec rax
   cmp rax, 0
   jge comparador
+  ;-problema en auxjump
   jmp auxJump
   
 izquierdaDesc:
   mov r8, izquierdaDesc
   mov r9, derechaAux
   add rax, 15
-  mov r11, [tablero+rax]
+  ;mov r11, [tablero+rax]--------------------
+  lea r11, [tablero]
+  add r11, rax
   mov [valor], r11b
   cmp byte [valor], 10
   je auxJump
   inc rax
   cmp rax, 82
+  ;-problema en comparador...
   jle comparador
   jmp auxJump
   
@@ -117,7 +180,9 @@ derechaAsc:
   mov r8, derechaAsc
   mov r9, derechaDesc
   sub rax, 13
-  mov r11, [tablero+rax]
+  ;mov r11, [tablero+rax]---------------
+  lea r11, [tablero]
+  add r11, rax
   mov [valor], r11b
   cmp byte [valor], 10
   je auxJump
@@ -130,7 +195,9 @@ derechaDesc:
   mov r8, derechaDesc
   mov r9, horizontalAux
   add rax, 13
-  mov r11, [tablero+rax]
+  ;mov r11, [tablero+rax]----------------
+  lea r11, [tablero]
+  add r11, rax
   mov [valor], r11b
   cmp byte [valor], 10
   je auxJump
@@ -146,7 +213,9 @@ horizontalIz:
   mov r8, horizontalIz
   mov r9, horizontalDer
   sub rax, 1
-  mov r11, [tablero+rax]
+  ;mov r11, [tablero+rax]------------------
+  lea r11, [tablero]
+  add r11, rax
   mov [valor], r11b
   cmp byte [valor], 10
   je auxJump
@@ -159,7 +228,9 @@ horizontalDer:
   mov r8, horizontalDer
   mov r9, verticalAux
   add rax, 1
-  mov r11, [tablero+rax]
+  ;mov r11, [tablero+rax]---------------------
+  lea r11, [tablero]
+  add r11, rax
   mov [valor], r11b
   cmp byte [valor], 10
   je auxJump
@@ -175,32 +246,47 @@ verticalDesc:
   mov r8, verticalDesc
   mov r9, continueSet
   add rax, 14
-  mov r11, [tablero+rax]
+  ;mov r11, [tablero+rax]--------------
+  lea r11, [tablero]
+  add r11, rax
   mov [valor], r11b
   cmp byte [valor], 10
   je auxJump
   cmp rax, 82
+  ;-problema en comparador...
   jle comparador
   jmp auxJump
   
 comparador:
-  mov bl, [tablero+rax]
+  ;mov bl, [tablero+rax]----------
+  lea r11, [tablero]
+  add r11, rax
   push rax
-  mov rax, [tempPosix]
-  mov cl, [tablero+rax]
+  xor rax, rax
+  mov al, [tempPosix]
+  ;mov cl, [tablero+rax]----------
+  push r10
+  lea r10, [tablero]
+  add r10, rax
+  cmp r11b, r10b
+  pop r10
   pop rax
-  cmp bl, cl
   je setMatch
   jne auxJump
   
 auxJump:
-  mov rax, [tempPosix]
+  mov al, [tempPosix]
+  ;-problemas en el jmp...
   jmp r9
+ret
   
 setMatch:
-  mov r11b, [match]
-  inc r11b
-  mov [match], r11b
+  push rax
+  xor rax, rax
+  mov al, [match]
+  inc al
+  mov [match], al
+  pop rax
   
 comprobarFichas:
   mov r11b, [match]
@@ -242,7 +328,10 @@ nuevoTableroAux:
   jmp inicio
   
 nuevoTablero:
-  mov [tablero+rax], byte "0"
+  ;mov [tablero+rax], byte "0"-------------
+  lea r11, [tablero]
+  add r11, rax
+  mov r11b, byte "0"
   sub rax, 2
   jmp nuevoTableroAux
 
@@ -250,25 +339,11 @@ nuevoTablero:
 impresion:
   ;mostrar el tablero
   ret
-  
-columnaLlena:
-  call resetIndice
-  ;print msgLleno, lenLleno
-  call impresion
-  jmp continue
  
 resetIndice:
   mov al, 5
   mov [indice], al
   ret
-  
-;cl contiene el parametro jugador...  
-one:
-  mov [jugador], byte 1
-  jmp continue ;jmp continue
-two:
-  mov [jugador], byte 2
-  jmp continue ;jmp continue
   
 fin:
   ret
--- Link a C++/main.cpp
+++ Link a C++/main.cpp
@@ -3,36 +3,33 @@
 #include <string>
 #include <cstring>
 #include <stdio.h>
+using namespace std;
 
 extern "C" void inicio();
 
 char dato[5];
-char tablero[47] = {'0','0', '0','0', '0','0', '0', '\n',
-				  '0','0', '0','0', '0','0', '0', '\n',
-				  '0','0', '0','0', '0','0', '0', '\n',
-				  '0','0', '0','0', '0','0', '0', '\n',
-				  '0','0', '0','0', '0','0', '0', '\n',
-				  '0','0', '0','0', '0','0', '0'};
-int jugador;
-int columna;
+char tablero[] = {'0', ' ','0', ' ', '0', ' ','0', ' ', '0', ' ','0', ' ', '0', '\n',
+				  '0', ' ','0', ' ', '0', ' ','0', ' ', '0', ' ','0', ' ', '0', '\n',
+				  '0', ' ','0', ' ', '0', ' ','0', ' ', '0', ' ','0', ' ', '0', '\n',
+				  '0', ' ','0', ' ', '0', ' ','0', ' ', '0', ' ','0', ' ', '0', '\n',
+				  '0', ' ','0', ' ', '0', ' ','0', ' ', '0', ' ','0', ' ', '0', '\n',
+				  '0', ' ','0', ' ', '0', ' ','0', ' ', '0', ' ','0', ' ', '0', '\n','\n'};			  
+int jugador = 2;
+int columna = 6;
 int casillasLlenas = 0;
 int indice = 5; //inicia siendo 0 por que es laposicion mas baja de la columna, se va llenando de arriba a abajo...
 
-
-void imp(){
-	std::cout << "hola";
+extern "C" int imp(){
+	std::cout << tablero;
+	return 0;
 }
-
+extern "C" int imp1(){
+	cout << "aaa" << endl;
+	return 0;
+}
 int main() {
-	/*
-	std::cout << "Ingrese una palabra de 4 letras..." << std::endl;
-	std::cin >> jugador;
+	//cout << tablero << endl << endl;
 	inicio();
-	std::cout << jugador << std::endl;
-	//tablero[0]=1;
-	*/
-	std::cout << tablero << std::endl << std::endl;
-	inicio();
-	std::cout << tablero << std::endl << std::endl;
+	//cout << tablero;
 	return 0;
 }
